e3f65f4f76b30afa80a9181b3daa1b302e244630
kangjiungdev <kangjiungdev@gmail.com>
2025-04-18 22:26:07 +0900

    2025-04-18 10:26

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,16 +3,14 @@
 import Link from "next/link";
 import { useUser } from "@/app/context/UserContext";
 
+// 404페이지도 usestate 사용 가능하게 설정해야함. 네비바가 ssr로
 export default function NotFoundPage() {
-  const { user } = useUser();
   return (
     <main
       className="p-8 mt-[72px] bg-[#343A40] flex flex-col items-center justify-center mt-[72px] min-h-[calc(100dvh-72px)] font-semibold"
       style={{ fontFamily: '"Spoqa Han Sans Neo", sans-serif' }}
     >
-      <h2 className="text-4xl mb-7">
-        {user?.id}404 - This page could not be found
-      </h2>
+      <h2 className="text-4xl mb-7">404 - This page could not be found</h2>
       <p>
         <Link
           href="/"
